cde2ed17882318e8f25068276eb38455868c46aa
Thomas Paul <tpaul19@wgu.edu>
2021-05-08 01:58:55 -0500

    test commit for push

--- src/main/java/com/quiz/Main.java
+++ src/main/java/com/quiz/Main.java
@@ -6,6 +6,6 @@ package com.quiz;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("I am starting to relearn Java");
     }
 }
